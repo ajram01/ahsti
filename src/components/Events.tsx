e615e171ca0aefc21406ee40da129143a7e11b8d
Salvador Pruneda <spruneda134@gmail.com>
2025-10-16 15:31:13 -0500

    Added pages

---
+++ src/components/Events.tsx
@@ -0,0 +1,35 @@
+import Event from "./event";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+export default function Events() {
+
+    return (
+        <div>
+        <Tabs defaultValue="upcoming" className="w-full pt-5">
+
+        <TabsList className="mx-auto">
+            <TabsTrigger value="upcoming">Upcoming Events</TabsTrigger>
+            <TabsTrigger value="previous">Previous Events</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="upcoming">
+            <div className="flex flex-row gap-3 mt-5 mb-3">
+                <Event />
+                <Event />
+            </div>
+            </TabsContent>
+
+        <TabsContent value="previous">
+            <div className="flex flex-row gap-3 mt-5 mb-3">
+                <Event />
+                <Event />
+            </div>
+        </TabsContent>
+
+        </Tabs>
+
+        </div>
+    );
+
+
+}
--- src/components/carousels/SubdivisionsCarousel.tsx
+++ src/components/carousels/SubdivisionsCarousel.tsx
@@ -1,24 +1,70 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay, Pagination, EffectFade } from "swiper/modules";
 import "swiper/css";
-import 'swiper/css/pagination';
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 import { Button } from "../ui/button";
 
 const items = [
-  { name: "Speedy Trails - Mission, TX", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" },
-  { name: "Speedy Trails - Mission, TX", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" },
-  { name: "Speedy Trails - Mission, TX", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" },
-  { name: "Speedy Trails - Mission, TX", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" },
-  { name: "Speedy Trails - Mission, TX", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" },
+  { 
+    name: "Crocket Estates",
+    location: "ALAMO, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/crockett-estates.png" 
+  },
+  { 
+    name: "Crown Haven",
+    location: "WESLACO, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/crown-haven.png" 
+  },
+  { 
+    name: "Jaguar Heights",
+    location: "EDINBURG, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/crockett-estates.png" 
+  },
+  { 
+    name: "Lakewood Estates",
+    location: "SAN JUAN, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/tiger-crossing.png" 
+  },
+  { 
+    name: "Mid Valley Estates",
+    location: "WESLACO, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/mid-valley-estates.png" 
+  },
+  { 
+    name: "Speedy Trails",
+    location: "MISSION, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/speedy-trails.png" 
+  },
+  { 
+    name: "Stonebriar",
+    location: "MCALLEN, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/crown-haven.png" 
+  },
+  { 
+    name: "TIGER CROSSING",
+    location: "MERCEDES, TX", 
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad", 
+    img: "/subdivisions/tiger-crossing.png" 
+  },
 ];
 
 export default function SubdivisionCarousel() {
   return (
-    <div className="w-full py-8">
+    <div className="w-full py-8 [--swiper-pagination-bullet-inactive-color:#FFFFFF!important]">
       <Swiper
-        modules={[Autoplay, Pagination]}
+        modules={[Autoplay, Pagination, EffectFade]}
+        effect="fade"
+        fadeEffect={{ crossFade: true }}
         spaceBetween={0}
         slidesPerView={1}
         loop={true}
@@ -27,27 +73,31 @@ export default function SubdivisionCarousel() {
           dynamicBullets: true,
         }}
         autoplay={{
-          delay: 5000, // continuous motion
+          delay: 5000,
           disableOnInteraction: false,
         }}
-        speed={1000} // smooth continuous scroll speed
-        freeMode={true}
-        allowTouchMove={true} // optional: disables manual dragging
+        speed={1000}
+        allowTouchMove={true}
       >
         {items.map((item, i) => (
           <SwiperSlide key={i}>
-            <div className="bg-[url(/subdivisions/speedy-trails.png)] bg-cover bg-no-repeat bg-center">
-            <div className="bg-gradient-to-b from-black/60 to-black/25 bg-blend-overlay">
+            <div
+              className="bg-cover bg-no-repeat bg-center"
+              style={{
+                backgroundImage: `url(${item.img})`, // 👈 use item image dynamically
+              }}
+            >
+              <div className="bg-gradient-to-b from-black/60 to-black/25 bg-blend-overlay">
                 <div className="w-[1140px] h-[600px] mx-auto justify-center flex flex-col text-center">
-                <h4 className="text-white">NONPROFIT • HUD-APPROVED COUNCELING</h4>
-                <h1 className="text-white">{item.name}</h1>
-                <p className="text-white w-2/3 mx-auto">{item.desc}</p>
-                <div className="flex flex-row gap-3 mx-auto mt-5">
-                    <Button className="w-3xs" size="lg">Learn More</Button>
-                    <Button className="w-3xs" size="lg" variant="secondary">All Subdivisions</Button>
+                  <h4 className="text-white">{item.location}</h4>
+                  <h2 className="text-white">{item.name}</h2>
+                  <p className="text-white w-2/3 mx-auto">{item.desc}</p>
+                  <div className="flex flex-row gap-3 mx-auto mt-5">
+            <Button className="w-[200px] py-6"  size="lg">Learn More</Button>
+            <Button className="w-[200px] py-6" size="lg" variant="secondary">All Subdivisions</Button>
+                  </div>
                 </div>
-                </div>
-            </div>
+              </div>
             </div>
           </SwiperSlide>
         ))}
